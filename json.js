7e6a908880ae54c19beed9940654c93a41ce7919
Janelle Salisbury <janellesalisbury@fullsail.edu>
2011-09-01 17:12:54 -0400

    no clue if this is right

--- json.js
+++ json.js
@@ -2,3 +2,30 @@
 // MiU 1109
 // Project 1
 
+var parts ={
+    "Brakes":{
+    "item": ["rotor", "caliper", "break pad", "lugnut"],
+    "qty":["2","1","4","6"],
+    "orderdate": ["2011-06-05","2011-06-05","2010-09-11","2011-09-11"]
+    },
+    "Engine":{
+    "item": ["water pump", "timing belt", "idler pulley", "crank seal"],
+    "qty":["1", "1", "1", "1"],
+    "orderdate": ["2011-07-07","2011-07-07", "2011-09-09", "2011-09-09"]
+    },
+    "Body":{
+    "item": ["hood", "quarter panel", "fender", "bumper"],
+    "qty": ["1", "2", "2", "2"],
+    "orderdate":["2011-09-09", "2011-10-02", "2011-11-11". "2011-12-23"]
+    },
+    "Lighting":{
+    "item":["dash lights", "head lights", "tail lights", "signal lights"],
+    "qty": ["1 set", "2", "2", "2 sets"],
+    "orderdate": ["2011-10-10", "2011-10-10", "2011-10-10". "2012-10-2"]
+    },
+    "Interior":{
+    "item":["seats", "carpeting", "console", "belts"],
+    "qty":["3", "1 roll", "1 unit", "5"],
+    "orderdate":["2012-01-01", "2012-01-25"," 2012-03-04","2011-09-23"]
+    };
+}
